4336eee504e0905c3bf624d7b6a0e2946353c62f
PABITRA OJHA <pabitraojha.444@gmail.com>
2025-12-11 14:15:56 +0000

    01 and 02 added successfully

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,23 @@
+/*to declare a variable we can use let, var, and for constant variable we can 
+use const.*/
+
+let var1 = "pabitra";
+/* here let is a keyword and myName is the name of variable and pabitra is 
+a string. let is a block scoped variable and we can't redeclare it.*/
+
+var name= "pabitra";
+/* here var is a keyword used to decalre a variable and it is a taraditional
+way to declare a variable and it has function scope and it can radeclare .
+*/
+
+const myName= "pabitra";
+/* here const keyword is used to declare a constant we can't change its value
+*/
+//myName= "ojha" // not allowed
+console.log(myName); //console.log is used for print 
+var1= 21;
+name = 23;
+var2= 3433; 
+console.table([var1,name, myName]); //used for print all variable in a table.
+
+let x; //undefined
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello");
+/*this is only used in html code and can run on browser but here we use nodejs 
+for running the js code that's why there is another syntax for alert*/
+
+/*--- datatypes ----
+a) Primitive Datatypes(7)
+    1.number 
+    2.String
+    3.Boolean (true / false)
+    4.Undefiend
+    5.Null
+    6.symbol
+    7.Bigint
+b) NOn-Primitive Datatypes
+    1.Object
+    2.Array
+    3.Function
+*/
+let age=23;
+typeof(age); //typeof operator used to find the type of a datatype.
+console.log(typeof null);//it will show object
+console.log(typeof undefined);//it will give undefiend
