f2cc9a63d790845973d716cfb61abed9918775c1
pabitraojha <pabitraojha.444@gmail.com>
2025-12-18 10:08:15 +0530

    edit IIFEE

--- 04_function/04_IIFE.js
+++ 04_function/04_IIFE.js
@@ -20,3 +20,30 @@ myFunction();
 ((name) => {
     console.log(`my name is ${name}`);
 })("Bablu");
+
+// ******** How javascript execute code **********//
+/* js executes cod eusing a single-threaded execution model with the help of execution context , call stack, web APIs , callback queue, microstack queue and event loop */
+// it first execute the asynchronous code then handles asynchronous code using the event loop mechanism.
+// Javascript engine: every browser has a javascript engine (V8 in chrome) .
+//this engine consists of   1) memory heap : stores variable and objects.
+//                            2) Call stack : execute code line by line
+
+//Execution context: whenever js runs a program, it creates an execution context.
+//mostly three types:    1) Global execution context : created first 
+//                       2) function execution context : created for each function call
+//                       3) Eval execution context 
+
+//Each execution context has : 1) variable environment(variables and functions)
+//                             2) lexical environment
+//                             3)This keyword
+
+//Two phases of execution : 
+// Phase-1: Memory creation phase (hoisting)
+// ->variables are allocated memory and set to undefined.
+// -> funtion declarations are stored completely.
+// -> No code is executed.
+// Phase-2: Code execution phase: 
+// -> code is executed line by line.
+// -> variables get actual value. 
+// -> Function are invoked. 
+
