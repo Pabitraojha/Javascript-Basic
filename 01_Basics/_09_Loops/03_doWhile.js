bb3770fd092e93e8e24c74adb6ef4586348a25a5
pabitraojha <pabitraojha.444@gmail.com>
2025-12-18 12:39:33 +0530

    add do while loop

---
+++ 01_Basics/_09_Loops/03_doWhile.js
@@ -0,0 +1,20 @@
+//do while loop 
+// Syntax
+// initialization
+//  do{ 
+//     do something>>>>>>;
+//      updation;
+//    }while(condition);
+
+let i= 0;
+do{
+    console.log(i);
+    i++;
+}while(i<19);
+
+let arr= [10,20,30,40,50,60,70];
+let index=0;
+do{
+    console.log(arr[index]);
+    index++;
+}while(index<arr.length);
