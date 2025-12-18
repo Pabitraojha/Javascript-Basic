6c282e69058dfe9dd60863b9f7b39e9e91c1a5eb
pabitraojha <pabitraojha.444@gmail.com>
2025-12-18 12:39:17 +0530

    added while loop

---
+++ 01_Basics/_09_Loops/02_While.js
@@ -0,0 +1,19 @@
+//while Loop  - Syntax
+//  initialization
+// while(condition){
+//     //do something
+//     //updation
+// }
+
+let i = 0;
+while (i<10){
+    console.log(i);
+    i++;
+}
+
+let arr= ["bablu", "Gudu" ,"Ojha"];
+let index=0;
+while(index<arr.length){
+    console.log(arr[index]);
+    index++;
+}
