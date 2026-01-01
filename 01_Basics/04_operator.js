779dfef06d5ac082a0cd6577ded7f02db760af47
pabitraojha <pabitraojha.444@gmail.com>
2026-01-01 11:17:58 +0530

    modified

--- 01_Basics/04_operator.js
+++ 01_Basics/04_operator.js
@@ -59,6 +59,6 @@ console.log("002" > 1); // true
 console.log(null > 0); // false
 console.log(null == 0); //false
 console.log(null >= 0 ); //true (here value conversion )
-/* here the reason is that an m equality chek == and comparisons >, < , >= , <= works differently. 
+/* here the reason is that an equality check == and comparisons >, < , >= , <= works differently. 
 comparisions convert null to a number, treating it as 0. That's why null >= 0 is ture and null > 0 is false.
 */
